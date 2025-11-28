abd671412cfb42136bf69901b73d2c853051d11e
DanDaDood <toffee.m.devera@gmail.com>
2025-11-28 17:16:51 +0800

    whoops
    all this time I havent passed one commit

---
+++ physics.py
@@ -0,0 +1,227 @@
+import math as m
+import pygame as pg
+import numpy as np
+
+pg.init()
+s_s = (400, 300)
+screen = pg.display.set_mode(s_s, pg.RESIZABLE)
+pg.display.set_caption("Physics Simulation")
+run = True
+
+# classes
+class ball: # for all ball objects
+  def __init__ (self, pos, vel, r, fmass, rot, rotv, fric, color):
+    self.pos = pg.Vector2(pos)
+    self.vel = pg.Vector2(vel)
+    self.r = r
+    self.rot = rot
+    self.rotv = rotv
+    self.fric = fric
+    self.color = color
+    self.fmass = fmass
+    self.mass = ((self.r**2)*m.pi)*self.fmass 
+  def __str__ (self):
+    return("".join((
+      "pos=(", str(self.pos),
+      "), vel=(", str(self.vel),
+      "), r=(", str(self.r),
+      "), rot=(", str(self.rot),
+      "), fmass=(", str(self.fmass),
+      "), rotv=(", str(self.rotv),
+      "), fric=(", str(self.fric),
+      "), mass=(", str(self.mass),
+      "),- color=(", str(self.color), ")")))
+
+class plane: # for all plane objects
+  def __init__ (self, pos, rot, fric, color):
+    self.pos = pg.Vector2(pos)
+    self.rot = rot
+    self.fric = fric
+    self.color = color
+  def __str__(self):
+    return("".join((
+      "pos=(", str(self.pos),
+      "), rot=(", str(self.rot),
+      "), fric=(", str(self.fric),
+      "),- color=(", str(self.color), ")")))
+
+class spring: # for all spring objects
+  def __init__ (self, ball_1, ball_2, length, stiff, damp, color):
+    self.ball_1 = ball_1
+    self.ball_2 = ball_2
+    self.stiff = stiff
+    self.damp = damp
+    self.color = color
+  def __str__(self):
+    return("".join((
+      "pos=(", str(self.pos),
+      "), rot=(", str(self.rot),
+      "), fric=(", str(self.fric),
+      "),- color=(", str(self.color), ")")))
+
+
+def vec_rot(rot):
+  return(pg.Vector2(m.cos(m.radians(rot)), m.sin(m.radians(rot))))
+
+def my_dot(pos, rot):
+  return(pos.dot(vec_rot(rot)))
+
+def scr_to_wrld(pos):
+  global c_p, c_z, s_s2
+  return(((pos - s_s2) / c_z) + c_p)
+
+def wrld_to_scr(pos):
+  global c_p, c_z, s_s2
+  return(((pos - c_p) * c_z) + s_s2)
+
+import math as m
+import pygame as pg
+
+def draw_half_plane(display, color, angle, position):
+
+  global c_p, c_z, s_s2
+  normal_x = m.cos(angle)
+  normal_y = -m.sin(angle)
+  normal_vector = pg.Vector2(normal_x, normal_y)
+  screen_width = display.get_width()
+  screen_height = display.get_height()
+  screen_corners = [pg.Vector2(0, 0), pg.Vector2(screen_width, 0), pg.Vector2(screen_width, screen_height), pg.Vector2(0, screen_height)]
+  world_corners = [scr_to_wrld(corner) for corner in screen_corners]
+  intersections = []
+  solid_corners = []
+  for i in range(4):
+    p1 = world_corners[i]
+    p2 = world_corners[(i + 1) % 4]
+    side_p1 = (p1 - position).dot(normal_vector)
+    side_p2 = (p2 - position).dot(normal_vector)
+    if (side_p1 * side_p2) < 0:
+      ratio = side_p1 / (side_p1 - side_p2)
+      intersections.append(p1.lerp(p2, ratio))
+    if side_p1 <= 0:
+      if p1 not in solid_corners: solid_corners.append(p1)
+  polygon_world = []
+  if len(intersections) == 2:
+    polygon_world.append(intersections[0])
+    polygon_world.extend(solid_corners)
+    polygon_world.append(intersections[1])
+  if len(polygon_world) >= 3:
+    polygon_screen = [wrld_to_scr(point) for point in polygon_world]
+    pg.draw.polygon(display, color, polygon_screen)
+
+# initialization
+
+scroll = 0 # scroll. is event.y 
+runtime = pg.time.Clock()
+
+precision = 20 # ticks per second. does not influence gametime. (turning this up will make it laggier, though.)
+
+# self explanatory physics stuff
+air_res = 0.99
+gravity = pg.Vector2(0, -1)
+enrg_loss = 0.8
+
+# set the stage
+c_p = pg.Vector2(0, 0) # camera position
+c_zr = 5 # real camera zoom
+
+c_zp = m.sqrt(2) # zoom exponential value
+c_zs = 0.01 # zoom scroll speed
+c_ps = 0.95 # dragoff velocity slowdown
+c_z = c_zp ** c_zr # real zoom
+c_vel = pg.Vector2(0) # camera dragoff velocity
+drag = 0 # dragging? (for drag)
+
+stage_color = (255, 255, 255)
+balls = []
+planes = []
+springs = []
+
+# add elements HERE
+balls.append(ball((5, 20), (0, 0), 5, 0.5, 0, 0, 0.5, (255,0,0)))
+balls.append(ball((10, 100), (0, 0), 2.5, 1, 0, 0, 0.5, (0,128,255)))
+planes.append(plane((0, 0), 95, 0, (255, 0, 128)))
+planes.append(plane((-100, 0), 0, 0, (255, 0, 0)))
+springs.append(springs(0, 1, 20, 0.8, 0.8, 0, (255, 0, 0)))
+
+while run:
+  # initialialize stuff
+  delta = runtime.tick(60)
+  s_s = pg.Vector2(screen.get_size())
+  s_s2 = s_s/2
+  screen.fill(stage_color)
+  m_pos = pg.Vector2(pg.mouse.get_pos()) - s_s2
+
+  # zoom
+  opr = c_p + (m_pos / c_z)
+  c_z = pg.math.lerp(c_z, (c_zp ** c_zr), min(c_zs * delta, 1))
+  c_p = opr - (m_pos / c_z)
+
+  for plane in planes:
+    draw_half_plane(screen, plane.color, m.radians(plane.rot), plane.pos)
+
+  for spring in springs:
+    L = spring.ball_1 - spring.ball_2
+
+  # ball render
+  for ball in balls:
+    ball.mass = ((ball.r ** 2) * m.pi) * ball.fmass
+    bfp = 1 + ball.fric
+    for i in range(precision):
+      ball.pos += ball.vel / precision
+      for plane in planes:
+        plane_vec = vec_rot(plane.rot)
+        dot = (ball.pos - plane.pos).dot(plane_vec) - ball.r
+        if dot <= 0:
+          ball.vel -= bfp * ball.vel.dot(plane_vec) * plane_vec
+          ball.pos -= plane_vec * dot
+      for hit_ball in balls:
+        
+        if ball == hit_ball:
+          continue
+        else:
+          d_vect = ball.pos - hit_ball.pos
+          dist = d_vect.length() 
+          radii_sum = ball.r + hit_ball.r
+          overlap = radii_sum - dist
+          if overlap > 0:
+            if dist == 0:
+              normal = pg.Vector2(0, 1) 
+            else:
+              normal = d_vect.normalize()
+            ball.pos += normal * (overlap / 2) 
+            hit_ball.pos -= normal * (overlap / 2)
+
+            v_n = (ball.vel - hit_ball.vel).dot(normal)
+            j = ((-(1 + enrg_loss) * v_n) / ((1 / ball.mass) + (1 / hit_ball.mass)))
+            J = j * normal
+
+            ball.vel += J / ball.mass 
+            hit_ball.vel -= J / hit_ball.mass 
+      ball.vel += gravity / precision
+      ball.vel *= air_res ** (1 / precision)
+    pg.draw.circle(screen, ball.color, wrld_to_scr((ball.pos.x, -ball.pos.y)), ball.r * c_z)
+  
+  pg.display.update()
+
+  scroll = 0
+  # events
+  for event in pg.event.get():
+    if event.type == pg.QUIT:
+      run = False
+    elif event.type == pg.MOUSEWHEEL:
+      scroll = event.y
+      c_zr += scroll
+
+  # zoom drag
+  if pg.mouse.get_pressed()[0]:
+    if drag == 0:
+      pg.mouse.get_rel()
+      drag = 1
+    else:
+      c_vel = pg.Vector2(pg.mouse.get_rel())
+      c_p -= (c_vel / c_z)
+  else:
+    c_vel *= c_ps**delta 
+    c_p -= c_vel / c_z
+    drag = 0
+pg.quit()
