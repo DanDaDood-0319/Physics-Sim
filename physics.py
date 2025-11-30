bf185fd71b903e7b94f802e6cc735421998939f4
DanDaDood <toffee.m.devera@gmail.com>
2025-11-30 12:55:43 +0800

    Added the ability to drag balls, and also fixed planes
    I probably wanna rename this PyGodoo

--- physics.py
+++ physics.py
@@ -1,25 +1,69 @@
 import math as m
 import pygame as pg
 import numpy as np
-
+import random as rndm # No, the collissions aren't random. It's for defaults.
+import time
 pg.init()
 s_s = (400, 300)
-screen = pg.display.set_mode(s_s, pg.RESIZABLE)
+screen = pg.display.set_mode(s_s, pg.RESIZABLE, pg.SRCALPHA)
 pg.display.set_caption("Physics Simulation")
 run = True
 
 # classes
 class ball: # for all ball objects
-  def __init__ (self, pos, vel, r, fmass, rot, rotv, fric, color):
-    self.pos = pg.Vector2(pos)
-    self.vel = pg.Vector2(vel)
-    self.r = r
+  """
+  A ball! What else is there to say?
+
+  Attributes
+  ----------
+  `pos`: a `pygame.Vector2` of the world position of the ball
+
+  `vel`: a `pygame.Vector2` of the world velocity of the ball
+
+  `r`: the radius of the ball in the world
+
+  `fmass`: how heavy the ball is _(NOTE: This is fake mass. The real mass is the attribute `mass`.)_
+
+  `rot`: the rotation of the ball
+
+  `rotv`: the rotational velocity of the all
+
+  `fric`: the ball's friction
+
+  `color`: a length 3 `tuple` with values within the range `[0 ... 255]`. Is the color.
+
+  Hidden Attributes
+  -----------------
+  `mass`: the true mass of the ball, calculated with `fmass * ((r ** 2) * m.pi)`.
+
+  Flags
+  -----
+  `isvisible`: determines if the ball is rendered]
+
+  `cancollideball`: determines if the ball collides with other balls
+
+  `cancollideplane`: determines if the ball collide with planes
+
+  `hasgravity`: is influenced by gravity
+  """
+  def __init__ (self,
+    pos = None, vel = None, r = None, fmass = None,
+    rot = 0, rotv = 0, fric = None, color = None,
+    isvisible = True, cancollideball = True, cancollideplane = True, hasgravity = True):
+    self.pos = pg.Vector2(rndm.uniform(-20, 20), rndm.uniform(-20, 20)) if pos == None else pg.Vector2(pos)
+    self.vel = pg.Vector2(rndm.uniform(-20, 20), rndm.uniform(-20, 20)) if vel == None else pg.Vector2(vel)
+    self.r = rndm.uniform(1.0, 20.0) if r == None else r
     self.rot = rot
     self.rotv = rotv
-    self.fric = fric
-    self.color = color
-    self.fmass = fmass
-    self.mass = ((self.r**2)*m.pi)*self.fmass 
+    self.fric = rndm.uniform(0.2, 5.0) if fric == None else fric
+    self.color = pg.Color(rndm.randint(0, 255), rndm.randint(0, 255), rndm.randint(0, 255), a = 255) if color == None else color
+    self.fmass = rndm.uniform(0.1, 20) if fmass == None else fmass
+    self.mass = ((self.r ** 2) * m.pi) * self.fmass
+    self.cancollideball = cancollideball
+    self.cancollideplane = cancollideplane
+    self.isvisible = isvisible
+    self.hasgravity = hasgravity
+
   def __str__ (self):
     return("".join((
       "pos=(", str(self.pos),
@@ -33,11 +77,26 @@ class ball: # for all ball objects
       "),- color=(", str(self.color), ")")))
 
 class plane: # for all plane objects
-  def __init__ (self, pos, rot, fric, color):
+  """
+  A plane for balls to collide with!
+
+  Attributes
+  ----------
+  `pos`: a `pygame.Vetor2` representing the position of the plane relative to the world.
+
+  `rot`: the angle in which the plane extends from (90° to the left is empty, and 90° to the right is filled.)
+
+  `fric`: the friction of the plane, for collision.
+  
+  `color`: a length 3 `tuple` with values within the range `[0 ... 255]`. Is the color. 
+  """
+  def __init__ (self, pos, rot, fric, color,
+  isvisible = True):
     self.pos = pg.Vector2(pos)
     self.rot = rot
     self.fric = fric
     self.color = color
+    self.isvisible = isvisible
   def __str__(self):
     return("".join((
       "pos=(", str(self.pos),
@@ -69,6 +128,12 @@ def vec_rot(rot):
 def my_dot(pos, rot):
   return(pos.dot(vec_rot(rot)))
 
+def divinf(a, b):
+  try:
+    return(a / b)
+  except:
+    return(m.inf)
+
 def scr_to_wrld(pos):
     global c_p, c_z, s_s2
     pos -= s_s2
@@ -81,13 +146,8 @@ def wrld_to_scr(pos):
     pos = pg.Vector2(pos.x, -pos.y)
     return(pos + s_s2)
 
-import math as m
-import pygame as pg
-
-def draw_half_plane(display, color, angle, position):
-  global c_p, c_z, s_s2
-  normal_x = m.cos(angle)
-  normal_y = m.sin(angle)
+def draw_half_plane(display, plane):
+  normal_x, normal_y = vec_rot(plane.rot)
   normal_vector = pg.Vector2(normal_x, normal_y)
   screen_width = display.get_width()
   screen_height = display.get_height()
@@ -98,8 +158,8 @@ def draw_half_plane(display, color, angle, position):
   for i in range(4):
     p1 = world_corners[i]
     p2 = world_corners[(i + 1) % 4]
-    side_p1 = (p1 - position).dot(normal_vector)
-    side_p2 = (p2 - position).dot(normal_vector)
+    side_p1 = (p1 - plane.pos).dot(normal_vector)
+    side_p2 = (p2 - plane.pos).dot(normal_vector)
     if (side_p1 * side_p2) < 0:
       ratio = side_p1 / (side_p1 - side_p2)
       intersections.append(p1.lerp(p2, ratio))
@@ -112,17 +172,37 @@ def draw_half_plane(display, color, angle, position):
     polygon_world.append(intersections[1])
   if len(polygon_world) >= 3:
     polygon_screen = [wrld_to_scr(point) for point in polygon_world]
-    pg.draw.polygon(display, color, polygon_screen)
+    pg.draw.polygon(display, plane.color, polygon_screen)
+  corner_sides = [(corner - plane.pos).dot(normal_vector) for corner in world_corners]
+  if all(side <= 0 for side in corner_sides):
+    display.fill(plane.color)
 
 def flip_y(pos):
   return(pg.Vector2(pos.x, -pos.y))
+
+def spring_ball(spring = 0, mouse=False, target=-1, damp=0.1):
+  global precision, balls, m_pos, c_p, c_z, gravity
+  if mouse:
+    ball_t = balls[target]
+    L = ((ball_t.pos + (ball_t.vel / damp)) - (gravity)) - ((m_pos / c_z) + c_p)
+    ball_t.vel -= ((L * damp) / precision)
+  else:
+    ball_1 = balls[spring.ball_1]
+    ball_2 = balls[spring.ball_2]
+    L = ball_1.pos - ball_2.pos
+    if L.length() != 0:
+      n = L.normalize()
+      force = (-spring.stiff * (L.length() - spring.length)) + (-spring.damp * (ball_1.vel - ball_2.vel).dot(n))
+      force *=  n
+      ball_1.vel += (force / ball_1.mass) / precision
+      ball_2.vel -= (force / ball_2.mass) / precision
 
 # initialization
 
 scroll = 0 # scroll. is event.y 
 runtime = pg.time.Clock()
 
-precision = 20 # ticks per second. does not influence gametime. (turning this up will make it laggier, though.)
+precision = 10 # ticks per second. does not influence gametime. (turning this up will make it laggier, though.)
 
 # self explanatory physics stuff
 air_res = 0.99
@@ -131,7 +211,7 @@ enrg_loss = 0.8
 
 # set the stage
 c_p = pg.Vector2(0, 0) # camera position
-c_zr = 5 # real camera zoom
+c_zr = 1 # real camera zoom
 
 c_zp = m.sqrt(2) # zoom exponential value
 c_zs = 0.01 # zoom scroll speed
@@ -140,17 +220,22 @@ c_z = c_zp ** c_zr # real zoom
 c_vel = pg.Vector2(0) # camera dragoff velocity
 drag = 0 # dragging? (for drag)
 
-stage_color = (255, 255, 255)
+stage_color = (100, 200, 255)
 balls = []
 planes = []
 springs = []
 
 # add elements HERE
-balls.append(ball((5, 20), (0, 0), 5, 0.5, 0, 0, 0.5, (255, 128, 0)))
-balls.append(ball((10, 100), (0, 0), 2.5, 1, 0, 0, 0.5, (0, 128, 255)))
+balls.append(ball((5, 20), (0, 100), 5, 0.5, 0, 0, 0.5, (255, 128, 0)))
+balls.append(ball((10, 100), (0, 0), 20, 1, 0, 0, 0.5, (0, 128, 255)))
+balls.append(ball((100, 100), (0, 0), 5, 1, 0, 0, 0.5, (0, 128, 255)))
+for i in range(0):
+  time.sleep(0.01)
+  balls.append(ball())
 planes.append(plane((0, 0), 95, 0, (255, 0, 128)))
 planes.append(plane((-100, 0), 0, 0, (255, 0, 0)))
-springs.append(spring(0, 1, 20, 0.8, 0.8, (0, 0, 0)))
+springs.append(spring(0, 1, 200, 0.8, 0.8, (0, 0, 0)))
+springs.append(spring(1, 2, 0, 0.99, 0.99, (0, 0, 0)))
 
 while run:
   # initialize stuff
@@ -166,59 +251,60 @@ while run:
   c_p = opr - (m_pos / c_z)
 
   for plane in planes:
-    draw_half_plane(screen, plane.color, m.radians(plane.rot), plane.pos)
+    if plane.isvisible == True:
+      draw_half_plane(screen, plane)
   
   # ball render
   for ball in balls:
     ball.mass = ((ball.r ** 2) * m.pi) * ball.fmass
     bfp = 1 + ball.fric
+
     for i in range(precision):
       ball.pos += ball.vel / precision
-      for plane in planes:
-        plane_vec = vec_rot(plane.rot)
-        dot = (ball.pos - plane.pos).dot(plane_vec) - ball.r
-        if dot <= 0:
-          ball.vel -= bfp * ball.vel.dot(plane_vec) * plane_vec
-          ball.pos -= plane_vec * dot
+      if ball.cancollideplane:
+        for plane in planes:
+          plane_vec = vec_rot(plane.rot)
+          dot = (ball.pos - plane.pos).dot(plane_vec) - ball.r
+          if dot <= 0:
+            ball.vel -= (bfp + plane.fric) * ball.vel.dot(plane_vec) * plane_vec
+            ball.pos -= plane_vec * dot
+
       for hit_ball in balls:
-        if ball == hit_ball:
+        if ball == hit_ball or ball.cancollideball == False or hit_ball.cancollideball == False:
           continue
-        else:
-          d_vect = ball.pos - hit_ball.pos
-          dist = d_vect.length() 
-          radii_sum = ball.r + hit_ball.r
-          overlap = radii_sum - dist
-          if overlap > 0:
-            if dist == 0:
-              normal = pg.Vector2(0, 1) 
-            else:
-              normal = d_vect.normalize()
-            ball.pos += normal * (overlap / 2) 
-            hit_ball.pos -= normal * (overlap / 2)
+        d_vect = ball.pos - hit_ball.pos
+        dist = d_vect.length() 
+        radii_sum = ball.r + hit_ball.r
+        overlap = radii_sum - dist
+        if overlap > 0:
+          if dist == 0:
+            normal = pg.Vector2(0, 1) 
+          else:
+            normal = d_vect.normalize()
+          ball.pos += normal * (overlap / 2) 
+          hit_ball.pos -= normal * (overlap / 2)
+          v_n = (ball.vel - hit_ball.vel).dot(normal)
+          j = ((-(1 + enrg_loss) * v_n) / ((1 / ball.mass) + (1 / hit_ball.mass)))
+          J = j * normal
+          ball.vel += J / ball.mass 
+          hit_ball.vel -= J / hit_ball.mass
 
-            v_n = (ball.vel - hit_ball.vel).dot(normal)
-            j = ((-(1 + enrg_loss) * v_n) / ((1 / ball.mass) + (1 / hit_ball.mass)))
-            J = j * normal
-
-            ball.vel += J / ball.mass 
-            hit_ball.vel -= J / hit_ball.mass
       for spring in springs:
-        ball_1 = balls[spring.ball_1]
-        ball_2 = balls[spring.ball_2]
-        L = ball_1.pos - ball_2.pos
-        if L.length() == 0:
-          continue
-        n = L.normalize()
-        force = (-spring.stiff * (L.length() - spring.length)) + (-spring.damp * (ball_1.vel - ball_2.vel).dot(n))
-        force *=  n
-        ball_1.vel += (force / ball_1.mass) / precision
-        ball_2.vel -= (force / ball_2.mass) / precision
-      ball.vel += gravity / precision
+        spring_ball(spring)
+      if drag > 0:
+        spring_ball(mouse = True, target = drag, damp=0.1)
+      if ball.hasgravity:
+        ball.vel += gravity / precision
       ball.vel *= air_res ** (1 / precision)
-    pg.draw.circle(screen, ball.color, wrld_to_scr(ball.pos), ball.r * c_z)
+    if ball.isvisible == True:
+      pg.draw.circle(screen, ball.color, wrld_to_scr(ball.pos), ball.r * c_z)
 
   for spring in springs:
     pg.draw.line(screen, spring.color, wrld_to_scr(balls[spring.ball_1].pos), wrld_to_scr(balls[spring.ball_2].pos), 5)
+  if drag > 0:
+    t_screen = pg.Surface(s_s, pg.SRCALPHA)
+    pg.draw.line(t_screen, pg.Color(255, 255, 255, 128), wrld_to_scr(balls[drag].pos), pg.mouse.get_pos(), 5)
+    screen.blit(t_screen, (0, 0))
 
   pg.display.update()
 
@@ -234,11 +320,15 @@ while run:
   # zoom drag
   if pg.mouse.get_pressed()[0]:
     if drag == 0:
-      pg.mouse.get_rel()
       drag = -1
+      for i, ball in enumerate(balls):
+        if ((((m_pos / c_z) + c_p) - ball.pos).length()) - ball.r <= 0:
+          drag = i
+      pg.mouse.get_rel()
     else:
-      c_vel = flip_y(pg.Vector2(pg.mouse.get_rel()))
-      c_p -= (c_vel / c_z)
+      if drag == -1:
+        c_vel = flip_y(pg.Vector2(pg.mouse.get_rel()))
+        c_p -= (c_vel / c_z)
   else:
     c_vel *= c_ps ** delta 
     c_p -= c_vel / c_z
